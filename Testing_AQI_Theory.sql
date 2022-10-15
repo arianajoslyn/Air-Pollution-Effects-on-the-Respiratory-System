4cd170ff09c45e5900f175c840f70f399af92454
Jermov <68399520+Jermov@users.noreply.github.com>
2022-10-14 21:50:46 -0500

    in a separate folder

--- Testing_AQI_Theory.sql
+++
@@ -1,8 +0,0 @@
---Testing a theory
---averages smooth the data out way too much
---a count of the occurences by the AQI_Index column may give insights
-
-SELECT a."Year",a."State Name",a."aqi_index",COUNT(a."aqi_index") as level_counts
-FROM "aqi_conversion" a
-WHERE a."aqi_index" IS NOT NULL
-GROUP BY a."State Name",a."Year", a."aqi_index";
